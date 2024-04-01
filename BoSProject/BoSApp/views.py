80fbc2fc7b988b7be51b4ef51fee8fa595513cf1
Enes Bera Engel <enes.engel@wechiptech.com>
2024-04-01 07:56:53 +0300

    html'ler eklendi loginpage için view oluşturuldu

--- BoSProject/BoSApp/views.py
+++ BoSProject/BoSApp/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request,'home.html')
+    return render(request,'BoSApp/home.html')
+
+def loginPage(request):
+    if request.method=='POST':
+        username= request.POST.get('username')
+        password= request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        #flash messages gelecek ve user modeli eklenmeli
+    context={}
+    return render(request, 'BoSApp/login_register.html', context)
