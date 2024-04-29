006fd291d19224463f34ae127b62b6e00d06cf6b
Enes Bera Engel <enes.engel@wechiptech.com>
2024-04-29 05:41:29 +0300

    pages for login, feed, owned_communities, create_community, enrolled_communities, all communities, single community are created their backends for viewing the communities and creating communities also implemented

--- BoSProject/BoSApp/urls.py
+++ BoSProject/BoSApp/urls.py
@@ -1,9 +1,10 @@
 from . import views
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from .views import UserProfileViewSet, CommunityViewSet, PostViewSet, FollowerViewSet, CommentViewSet, VoteViewSet, OwnerViewSet, ModeratorViewSet, CommunityMemberViewSet, CommunityBannedUserViewSet, UserBlockedUserViewSet, PostFormatViewSet, MembershipRequestViewSet, FollowRequestViewSet
+from .views import OwnerCommunityViewSet, UserCommunityViewSet, UserProfileViewSet, CommunityViewSet, PostViewSet, FollowerViewSet, CommentViewSet, UserViewSet, VoteViewSet, OwnerViewSet, ModeratorViewSet, CommunityMemberViewSet, CommunityBannedUserViewSet, UserBlockedUserViewSet, PostFormatViewSet, MembershipRequestViewSet, FollowRequestViewSet
 
 router = DefaultRouter()
+router.register(r'users', UserViewSet)
 router.register(r'userprofiles', UserProfileViewSet)
 router.register(r'owners', OwnerViewSet)
 router.register(r'moderators', ModeratorViewSet)
@@ -18,6 +19,10 @@ router.register(r'communities', CommunityViewSet)
 router.register(r'postformats', PostFormatViewSet)
 router.register(r'membershiprequests', MembershipRequestViewSet)
 router.register(r'followrequests', FollowRequestViewSet)
+router.register(r'my-communities', UserCommunityViewSet,
+                basename='my-communities')
+router.register(r'owned-communities', OwnerCommunityViewSet,
+                basename='owned-communities')
 
 urlpatterns = [
     path('', include(router.urls)),
@@ -25,6 +30,27 @@ urlpatterns = [
     path('api/check_email/', views.check_email, name='check_email'),
     path('create_user/', views.create_user_view, name='create_user'),
     path('home', views.home, name="home"),
-    path('login/', views.home, name="login"),
+    path('loginPage/', views.loginPage_view, name="loginPage"),
+    path('feed/', views.feed_view, name="feed"),
+    path('login/', views.login_view, name="login"),
+    path('logout/', views.logout_view, name="logout"),
+    path('profile/', views.profile_page_view, name="profile"),
+    path('community/', views.community_page_view, name="community"),
+    path('create_community/', views.create_community_view, name="create_community"),
+    path('enrolled_communities/', views.enrolled_communities_view,
+         name="enrolled_communities"),
+    path('enrolled_communities_page/', views.enrolled_communities_page_view,
+         name="enrolled_communities_page"),
+    path('owned_communities/', views.owned_communities_view,
+         name="owned_communities"),
+    path('my_posts/', views.my_posts_view, name="my_posts"),
+    path('single_community/', views.single_community_page,
+         name='single_community'),
+    path('create_community_page/', views.create_community_page_view,
+         name='create_community_page'),
+    path('create_posttype_page/', views.create_posttype_page_view,
+         name='create_posttype_page'),
+    path('create_post_page/', views.create_post_page_view, name='create_post_page'),
+
     # path('create/', views.create_user, name='create_user'),
 ]
