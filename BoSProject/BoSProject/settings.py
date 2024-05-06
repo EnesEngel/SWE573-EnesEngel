e8847199001d13d8e5a87bc3d1444e4b944b7d1d
Enes Bera Engel <enes.engel@wechiptech.com>
2024-05-06 20:25:28 +0300

    bug fix trial 5 added rest_framework authentication classes in settings

--- BoSProject/BoSProject/settings.py
+++ BoSProject/BoSProject/settings.py
@@ -82,7 +82,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'BoSProject.wsgi.application'
 
-
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
